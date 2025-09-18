0b92fd12a8a762258dffd2e23f65862eec74113b
lokman-hosen <lokman@mcc.com.bd>
2025-09-18 18:30:02 +0600

    Donation and wise page link updated by route

--- resources/js/Components/Donation/CTA.jsx
+++ resources/js/Components/Donation/CTA.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from "@inertiajs/react";
 
 const CTA = () => {
     return (
@@ -7,10 +8,12 @@ const CTA = () => {
                 <h2 className="text-3xl font-bold mb-6">Want to See More?</h2>
                 <p className="text-xl mb-8 max-w-3xl mx-auto">Sign up for a free account to view all donations and
                     connect with donors in your community</p>
-                <a href="registration-new.html"
-                   className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-purple-600 bg-white hover:bg-gray-100">
+
+                <Link
+                    href={route('register')}
+                    className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-purple-600 bg-white hover:bg-gray-100">
                     Create Free Account
-                </a>
+                </Link>
             </div>
         </section>
     );
--- resources/js/Components/Wish/CTA.jsx
+++ resources/js/Components/Wish/CTA.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from "@inertiajs/react";
 
 const CTA = () => {
     return (
@@ -7,10 +8,12 @@ const CTA = () => {
                 <h2 className="text-3xl font-bold mb-6">Want to Help?</h2>
                 <p className="text-xl mb-8 max-w-3xl mx-auto">Sign up for a free donor account to fulfill wishes and
                     make children's dreams come true</p>
-                <a href="registration-new.html"
-                   className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-purple-600 bg-white hover:bg-gray-100">
+
+                <Link
+                    href={route('register')}
+                    className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-purple-600 bg-white hover:bg-gray-100">
                     Become a Donor
-                </a>
+                </Link>
             </div>
         </section>
     );
