88cdea183cc3fdcfdfb66dcd9e2ab066a9c293c3
lokman-hosen <lokman@aspiretss.com>
2025-09-26 16:31:46 +0600

    User list added in admin panel

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Models\User;
+use Inertia\Response;
+
+class UserController extends Controller
+{
+    const moduleDirectory = 'Admin/User/';
+    const moduleName = 'User List';
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Response
+    {
+        $users = User::orderBy('id', 'asc')->paginate(5); // 5 per page
+        return inertia(self::moduleDirectory.'Index', [
+            'module' => self::moduleName,
+            'users' => $users
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
