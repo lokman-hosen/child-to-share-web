ee3e574d699538478743c6a5e4a13fa56a7727df
lokman-hosen <lokman@mcc.com.bd>
2025-10-05 16:32:28 +0600

    Dashboard and website forward menu added

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import {Head, Link} from "@inertiajs/react";
-import {faCheck, faGift, faHandshake, faStar, faUsers} from '@fortawesome/free-solid-svg-icons';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import Hero from "@/Components/Home/Hero.jsx";
 import Summary from "@/Components/Home/Summary.jsx";
 import HowWorks from "@/Components/Home/HowWorks.jsx";
-import WishList from "@/Components/Home/WishList.jsx";
 import DonationList from "@/Components/Home/DonationList.jsx";
-import OrganizationList from "@/Components/Home/OrganizationList.jsx";
 import RegistrationCTA from "@/Components/Home/RegistrationCTA.jsx";
 import Faq from "@/Components/Home/Faq.jsx";
 import Footer from "@/Components/Common/Footer.jsx";
