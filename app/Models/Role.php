7991702085e94e5df732e76f58da3641d71372fc
lokman-hosen <lokman@mcc.com.bd>
2025-11-05 15:27:30 +0600

    Some migration updated

---
+++ app/Models/Role.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Role extends Model
+{
+    /** @use HasFactory<\Database\Factories\RoleFactory> */
+    use HasFactory,SoftDeletes;
+
+    protected $guarded = ['id'];
+}
