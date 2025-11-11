9ed4899fae2842459e36f6acf0081f58b5c15f46
lokman-hosen <lokman@aspiretss.com>
2025-11-11 09:15:30 +0600

    Added code insert donor and wisher info to user table

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use App\Services\DonationService;
 use App\Services\WishService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 use Inertia\Response;
+use function Laravel\Prompts\table;
 
 class DashboardController extends Controller
 {
@@ -21,6 +24,33 @@ class DashboardController extends Controller
 
     public function index(): Response
     {
+        // insert donor info to user table
+//        $donors = DB::table('donors')->get();
+//        foreach ($donors as $donor){
+//            User::where('id', $donor->user_id)->update([
+//                'dob' => $donor->dob,
+//                'guardian_name' => $donor->guardian_name ?? null,
+//                'guardian_phone' => $donor->guardian_phone ?? null,
+//                'relationship' => $donor->relationship ?? null,
+//                'gender' => $donor->gender ?? 'male',
+//                'email_verified_at' => now(),
+//                'phone_verified_at' => now(),
+//            ]);
+//        }
+        // insert wisher info to the user table
+//        $wishers = DB::table('wishers')->get();
+//        foreach ($wishers as $wisher){
+//            User::where('id', $wisher->user_id)->update([
+//                'dob' => $wisher->dob,
+//                'guardian_name' => $wisher->guardian_name ?? null,
+//                'guardian_phone' => $wisher->guardian_phone ?? null,
+//                'relationship' => $wisher->relationship ?? null,
+//                'gender' => $wisher->gender ?? 'male',
+//                'email_verified_at' => now(),
+//                'phone_verified_at' => now(),
+//            ]);
+//        }
+
         // donation
         $availableDonationCount = $this->donationService
             ->donationByStatus('available', 'count',  null,'admin');
