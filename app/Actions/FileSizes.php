10eeb16087019070bec41837eccab3770aaf0532
lokman-hosen <lokman@mcc.com.bd>
2025-11-13 12:49:25 +0600

    Worked on image upload resize

--- app/Actions/FileSizes.php
+++ app/Actions/FileSizes.php
@@ -5,6 +5,6 @@ namespace App\Actions;
 class FileSizes
 {
     //const fileSize = ['width' => 400, 'height' => 300];
-    const PROFILE_IMAGE = ['path' => 'photos','width' => 390, 'height' => 250];
+    const PROFILE_IMAGE = ['path' => 'photos','width' => 390, 'height' => 245];
 
 }
