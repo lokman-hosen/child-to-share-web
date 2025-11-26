0d55473b50458f4a4bd021ada2ce360091e968fd
lokman-hosen <lokman@mcc.com.bd>
2025-11-26 16:47:49 +0600

    Card updated

--- resources/js/Components/Home/DonationList.jsx
+++ resources/js/Components/Home/DonationList.jsx
@@ -19,7 +19,7 @@ const DonationList = ({donations, user}) => {
                         </Link>
                     </div>
                     {donations.length > 0 ? (
-                        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+                        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5">
                             {donations.map((donation, index) => (
                                 <SingleDonationItem key={index} donation={donation}/>
                             ))}
