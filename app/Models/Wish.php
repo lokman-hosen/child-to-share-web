6cdfb9bfcc6f0a2ca8c4476120bd9617e27bea54
lokman-hosen <lokman@aspiretss.com>
2025-12-06 13:15:27 +0600

    Fulfil wish form added

--- app/Models/Wish.php
+++ app/Models/Wish.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -93,5 +94,10 @@ class Wish extends Model
     {
         return $this->morphMany(File::class, 'fileable')
             ->where('file_type', 'video');
+    }
+
+    public function fulfillments(): HasMany
+    {
+        return $this->hasMany(Fulfillment::class);
     }
 }
