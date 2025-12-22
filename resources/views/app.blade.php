278df69225a21f9c73873e560e4874d86ea0a804
lokman-hosen <lokman@mcc.com.bd>
2025-12-22 13:09:12 +0600

    Fav icon added

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,10 @@
 
     <title inertia>{{ config('app.name', 'ThreeWish') }}</title>
     <!-- Fonts -->
+    <link rel="icon" sizes="180x180" href="{{asset('images/fav-icon.png')}}" />
+    <meta name="apple-mobile-web-app-title" content="threeWish" />
+    <link rel="manifest" href="http://threewish.org/" />
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
