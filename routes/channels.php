37077bab063ae3fb5faf1e70229ba9cf294be44a
lokman-hosen <lokman@mcc.com.bd>
2025-12-28 12:47:36 +0600

    Continue chatting

--- routes/channels.php
+++ routes/channels.php
@@ -17,9 +17,10 @@ use Illuminate\Support\Facades\Broadcast;
 //    return (int) $user->id === (int) $id;
 //});
 
-Broadcast::channel('fulfilment.{fulfilmentId}', function ($user, $fulfilmentId) {
+Broadcast::channel('chat.{fulfilmentId}', function ($user, $fulfilmentId) {
     return \App\Models\Fulfillment::where('id', $fulfilmentId)
         ->whereHas('wish', fn ($q) => $q->where('user_id', $user->id))
         ->orWhereHas('donation', fn ($q) => $q->where('user_id', $user->id))
         ->exists();
 });
+
