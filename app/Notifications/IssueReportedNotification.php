711e2edb718955957c772f534905187b3dda6659
lokman-hosen <lokman@mcc.com.bd>
2026-01-06 14:24:18 +0600

    Queued all mail and notification

--- app/Notifications/IssueReportedNotification.php
+++ app/Notifications/IssueReportedNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class IssueReportedNotification extends Notification
+class IssueReportedNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
--- app/Notifications/ItemReceivedNotification.php
+++ app/Notifications/ItemReceivedNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class ItemReceivedNotification extends Notification
+class ItemReceivedNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
