12471461bea65c749d31952d5e326679fd0a86e4
lokman-hosen <lokman@aspiretss.com>
2026-01-24 13:27:13 +0600

    Organization update done

--- resources/js/Pages/Admin/Organization/Edit.jsx
+++ resources/js/Pages/Admin/Organization/Edit.jsx
@@ -34,7 +34,7 @@ export default function Edit({module,organization,statuses}) {
 
                             {/* Action Button */}
                             <Link
-                                href={route('donations.index')}
+                                href={route('organizations.index')}
                                 className="inline-flex items-center justify-center space-x-2 bg-white text-blue-600 hover:bg-blue-50 font-semibold py-3 px-6 rounded-lg shadow-md hover:shadow-lg transition-all duration-300 transform hover:scale-105 border border-white border-opacity-30"
                             >
                                 <FontAwesomeIcon icon={faList} className="text-blue-600" />
